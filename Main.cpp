b6e3629d4573ed935b5f0a8a3a824a9bf3257f96
lgarcia77021 <l_garcia77021@outlook.com>
2021-06-07 14:39:54 -0500

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include "rangen.h"
 using namespace std;
 
 int main() {
@@ -12,9 +13,13 @@ cout << " Hello, greetings. Please state your name " << endl;
 getline(cin,NAME);
 
 cout << " Greetings " << NAME << endl;
+
+
+
+ RNG();
  
  return 0;
  
  
- 
+
 }
