eb6b93d6f1bb0596a0395226a5d8fbf47ad41fe1
lgarcia77021 <l_garcia77021@outlook.com>
2021-06-07 00:47:11 -0500

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -1,11 +1,17 @@
 #include <iostream>
-
+#include <string>
 using namespace std;
 
 int main() {
     
+string FN;
+    
     
 cout << " Hello, greetings. Please state your name " << endl;
+
+getline(cin,FN);
+
+cout << " Greetings " << FN << endl;
  
  return 0;
  
