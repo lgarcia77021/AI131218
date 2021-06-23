78387f633259f29f162f5348035258291c4f7a4f
lgarcia77021 <l_garcia77021@outlook.com>
2021-06-22 23:08:10 -0500

    Update rangen.cpp

--- rangen.cpp
+++ rangen.cpp
@@ -1,11 +1,23 @@
 #include <iostream>
+#include <cstdio>     
+#include <cstdlib>   
+#include <ctime>       
 using namespace std;
 
 int RNG()
 {
     
+    int RGX;
     
     cout << " RNG online " << endl;
+    
+
+  srand (time(NULL));
+
+
+  RGX = rand() % 100000000;
+  
+    cout << RGX << endl;
     
     return 0;
     
