8a14371c5d9a50e21b84b6c313906bd4e7b2b33f
lgarcia77021 <l_garcia77021@outlook.com>
2021-06-23 22:55:07 -0500

    Update rangen.cpp

--- rangen.cpp
+++ rangen.cpp
@@ -6,15 +6,9 @@ using namespace std;
 
 int main()
 {
-    
-    
     int RGX;
-
-    
     srand (time(NULL));
-
-
-  RGX = rand() % 100000000 + 9999999;
+    RGX = rand() % 100000000 + 9999999;
   
 cout << RGX << endl;
 
@@ -23,11 +17,7 @@ while(RGX!=1 || 0)
     
     int m = RGX % 2;
     cout << " " << m << " ";
-    
-    
-RGX = RGX / 2;
-
-
+    RGX = RGX / 2;
 cout << RGX << endl;
 
 }  
