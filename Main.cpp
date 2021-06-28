386759719d37f8e5b17263d6d98e03f1fd35de6e
lgarcia77021 <l_garcia77021@outlook.com>
2021-06-27 23:36:03 -0500

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -14,7 +14,19 @@ getline(cin,NAME);
 
 cout << " Greetings " << NAME << endl;
 
+for (int i = 0; NAME[i] != '\0'; i++) {
+        int v = int(NAME[i]);
+        while (v != 1 || 0) {
+            int m = v % 2;
+            cout << " " << m << " ";
+            v = v / 2;
+        }
+        cout << " " << v;
+        
 
+    }
+
+cout << " " << endl;
 
  RNG();
  
