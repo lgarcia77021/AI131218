61af2b8757300e644bb88a5c4104d75bbd722a5b
lgarcia77021 <l_garcia77021@outlook.com>
2021-07-01 15:36:17 -0500

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -1,30 +1,57 @@
 #include <iostream>
-#include <string>
 #include "rangen.h"
+#include <array>
 using namespace std;
 
-int main() {
-    
-string NAME;
-    
-    
+int main()
+{
+char NAME[128];
+int x = 0;
+int v;
+int z = 0;
+int r[8];
+
+
+
 cout << " Hello, greetings. Please state your name " << endl;
 
-getline(cin,NAME);
+cin >> NAME;
 
-cout << " Greetings " << NAME << endl;
 
-for (int i = 0; NAME[i] != '\0'; i++) {
-        int v = int(NAME[i]);
-        while (v != 1 || 0) {
-            int m = v % 2;
-            cout << " " << m << " ";
-            v = v / 2;
-        }
-        cout << " " << v;
-        
 
+while (NAME[x] !='\0')    // While the string isn't at the end... 
+{
+
+    v = int(NAME[x]);
+    
+     while (v != 1 || 0) {
+
+    int m = v % 2;
+    cout << " " << m << " ";
+    v = v / 2;
+    z = z + 1;
+    
+    cout << "z" << z << endl;
+    r[z] = m;
+
+    if (v == 1||0)
+    {
+       cout << " "<< v ;
     }
+}
+
+ x++;
+ 
+ 
+
+}
+
+for(int i=8;i>=1;i--)
+{
+    
+    cout<< r[i] << endl;
+    
+}
 
 cout << " " << endl;
 
